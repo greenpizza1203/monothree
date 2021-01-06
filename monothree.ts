bd226c1d83d44e299b737d004d09611e5cbe58b3
greenpizza1203 <konakarthik12@gmail.com>
2021-01-06 12:32:41 -0500

    changes

---
+++ monothree.ts
@@ -0,0 +1,24 @@
+export {WebGLRenderer} from 'three/src/renderers/WebGLRenderer';
+export {Scene} from 'three/src/scenes/Scene';
+export {Mesh} from 'three/src/objects/Mesh';
+
+export {MeshPhongMaterial} from 'three/src/materials/MeshPhongMaterial';
+
+
+export {SpotLight} from 'three/src/lights/SpotLight';
+
+
+export {AmbientLight} from 'three/src/lights/AmbientLight';
+
+
+export {PerspectiveCamera} from 'three/src/cameras/PerspectiveCamera';
+export {CanvasTexture} from "three/src/textures/CanvasTexture"
+export {Geometry} from 'three/src/core/Geometry';
+export {Face3} from 'three/src/core/Face3';
+
+export {Sphere} from 'three/src/math/Sphere';
+
+export {Quaternion} from 'three/src/math/Quaternion';
+export {Vector4} from 'three/src/math/Vector4';
+export {Vector3} from 'three/src/math/Vector3';
+export {Vector2} from 'three/src/math/Vector2';
